a79574923d10157a899786a2e321f27d39f245d2
Sandra Padua <sandrapadua5@gmail.com>
2019-02-13 14:14:00 +0100

    Display elements and reset form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -14,5 +14,11 @@ const submitComment = () => {
     comment.appendChild(h3)
     comment.appendChild(p)
     console.log(comment)
+    const commentSection = document.getElementById("comments")
+    console.log(commentSection);
+    commentSection.appendChild(comment)
+    inputField.value = null
+    textArea.value = null
+
 
 }
