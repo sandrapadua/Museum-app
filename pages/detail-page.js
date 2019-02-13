444c83e187083c9686d7f9b96907d2131e7d4786
Sandra Padua <sandrapadua5@gmail.com>
2019-02-13 15:54:53 +0100

    set up form validation

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,10 +1,17 @@
 const submitComment = () => {
     const inputField = document.getElementById('name')
-    const name = inputField.value
+    let name = inputField.value
     console.log(name)
     const textArea = document.getElementById('msg')
     const msg = textArea.value
     console.log(msg)
+    if(name.charAt(0)!== name.charAt(0).touppercase){
+    name = name.charAt(0).toUpperCase() + name.slice(1);
+    }
+    if(doesNotPassAllValidations(name,msg)){
+       return null; 
+    }
+    else{
     const comment = document.createElement('section')
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
@@ -14,11 +21,35 @@ const submitComment = () => {
     comment.appendChild(h3)
     comment.appendChild(p)
     console.log(comment)
+
     const commentSection = document.getElementById("comments")
     console.log(commentSection);
     commentSection.appendChild(comment)
     inputField.value = null
     textArea.value = null
-
+    }
+    function doesNotPassAllValidations(name,msg){
+        if(!name){
+            alert('You forgot to fill in your name!!')
+            return true
+        }
+        if(!msg){
+            alert('You forgot to fill in your message!!')
+            return true
+        }
+        if(msg.length > 280){
+            alert('comment is too long!')
+            return true   
+        }
+        var letters = /^[a-zA-Z ]+$/;
+        if(msg.match(letters)){
+            return false  
+        }
+        else{
+            alert('Warning: this comment has been flagged as offensive')
+            return true   
+        }
+        return false
+    }
 
 }
